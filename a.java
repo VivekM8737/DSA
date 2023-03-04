d353980a77bdd2bde7f4a09ba1f6b728310711c7
Vivek <mukandarkasikandar8382@gmail.com>
2023-03-04 19:02:42 +0530

    Initial commit

---
+++ a.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class a{
+public static void main(String[] args) {
+    System.out.println("Hii");
+    Scanner s=new Scanner(System.in);
+    int x=s.nextInt();
+    System.out.println(x);
+    System.out.println("end");
+
+}
+
+}
